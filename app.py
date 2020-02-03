8fb0e4c103cccee0c8defc2832ff913af7f4ed91
Ramko Nanjul <ramkodgreat@gmail.com>
2020-02-03 12:11:10 +0100

    uploading updated changes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 api = Api(app)
 
 # configurations 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or"sqlite:///generate.sqlite3"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "sqlite:///generate.sqlite3"
 app.config['SECRET_KEY'] = "abcd1234"
 
 
